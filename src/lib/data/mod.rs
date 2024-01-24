c29c3e90f7daebe1bbccf75ec7662532442ad63f
Giedrius Išora <g.isora@outlook.com>
2024-01-24 22:17:55 +0200

    add and impl Time/ClipId structs

---
+++ src/lib/data/mod.rs
@@ -0,0 +1,30 @@
+use derive_more::{Display, From};
+use serde::{Deserialize, Serialize};
+use uuid::Uuid;
+use std::str::FromStr;
+
+#[derive(Debug, Clone, From, Display, Deserialize, Serialize)]
+pub struct DbId(Uuid);
+
+impl DbId {
+    pub fn new() -> Self {
+        Uuid::new_v4().into()
+    }
+
+    pub fn nill() -> Self {
+        DbId(Uuid::nil())
+    }
+}
+
+impl Default for DbId {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+impl FromStr for DbId {
+    type Err = uuid::Error;
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        Ok(Self(Uuid::parse_str(s)?))
+    }
+}
--- src/lib/domain/time.rs
+++ src/lib/domain/time.rs
@@ -1,4 +1,4 @@
-use chrono::{Date, DateTime, NaiveDateTime, Utc};
+use chrono::{DateTime, NaiveDateTime, Utc};
 use derive_more::From;
 use serde::{Deserialize, Serialize};
 use std::str::FromStr;
