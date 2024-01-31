2945574bb7d9d41d340ca4f2567088819bf7aa1b
Giedrius Išora <g.isora@outlook.com>
2024-01-31 21:21:39 +0200

    impl delete_expired

--- src/bin/httpd.rs
+++ src/bin/httpd.rs
@@ -1,6 +1,7 @@
 use clipstash::data::AppDatabase;
 use clipstash::web::hitcounter::HitCounter;
 use clipstash::web::renderer::Renderer;
+use clipstash::domain::maintenance::Maintenance;
 use dotenv::dotenv;
 use std::path::PathBuf;
 use structopt::StructOpt;
@@ -21,17 +22,16 @@ fn main() {
 
     let rt = tokio::runtime::Runtime::new().expect("failed to spawn tokio runtime");
     let handle = rt.handle().clone();
-
     let renderer = Renderer::new(opt.template_directory.clone());
-
     let database = rt.block_on(async move { AppDatabase::new(&opt.connection_string).await });
-
     let hit_counter = HitCounter::new(database.get_pool().clone(), handle.clone());
+    let maintenance = Maintenance::spawn(database.get_pool().clone(), handle.clone());
 
     let config = clipstash::RocketConfig {
         renderer,
         database,
         hit_counter,
+        maintenance,
     };
 
     rt.block_on(async move {
--- src/lib/mod.rs
+++ src/lib/mod.rs
@@ -8,6 +8,7 @@ pub use data::DataError;
 pub use domain::clip::field::ShortCode;
 pub use domain::clip::{Clip, ClipError};
 pub use domain::time::Time;
+pub use domain::maintenance::Maintenance;
 pub use service::ServiceError;
 use web::{renderer::Renderer, hitcounter::HitCounter};
 use rocket::fs::FileServer;
@@ -17,6 +18,7 @@ pub struct RocketConfig {
     pub renderer: Renderer<'static>,
     pub database: AppDatabase,
     pub hit_counter: HitCounter,
+    pub maintenance: Maintenance,
 }
 
 pub fn rocket(config: RocketConfig) -> Rocket<Build> {
@@ -24,6 +26,7 @@ pub fn rocket(config: RocketConfig) -> Rocket<Build> {
         .manage::<AppDatabase>(config.database)
         .manage::<Renderer>(config.renderer)
         .manage::<HitCounter>(config.hit_counter)
+        .manage::<Maintenance>(config.maintenance)
         .mount("/", web::http::routes())
         .mount("/api/clip", web::api::routes())
         .mount("/static", FileServer::from("static"))
